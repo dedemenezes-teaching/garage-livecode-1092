6813b0f52b16a81012d37b0969c4b329458cd66d
Andre Menezes <djdedemenezes@gmail.com>
2022-12-01 20:01:50 -0300

    feat: fetch all cars and add car

--- src/controllers/garage_controller.js
+++ src/controllers/garage_controller.js
@@ -1,9 +1,58 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["carsList"]
+  static targets = ['carsList', 'brand', 'model', 'owner', 'plate']
 
   connect() {
-    console.log("hello from garage controller!")
+    // console.log(this.element);
+    // console.log(this.carsListTarget);
+    this.fetchAllCars()
   }
-}
+
+  fetchAllCars() {
+    fetch('https://wagon-garage-api.herokuapp.com/awesome-1092/cars')
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
+        data.forEach((car) => {
+          console.log(car);
+          this.insertCarCard(car)
+        })
+      })
+  }
+
+  insertCarCard(car) {
+    this.carsListTarget.insertAdjacentHTML('beforeend', `<div class="car">
+          <div class="car-image">
+            <img src="http://loremflickr.com/280/280/${car.brand} ${car.model}" />
+          </div>
+          <div class="car-info">
+            <h4>${car.brand} ${car.model}</h4>
+            <p><strong>Owner:</strong> ${car.owner}</p>
+            <p><strong>Plate:</strong> ${car.plate}</p>
+          </div>
+        </div>`)
+  }
+  submitCar(event) {
+    event.preventDefault()
+    console.log(event.currentTarget)
+    fetch('https://wagon-garage-api.herokuapp.com/awesome-1092/cars', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        "brand": this.brandTarget.value,
+        "model": this.modelTarget.value,
+        "owner": this.ownerTarget.value,
+        "plate": this.plateTarget.value
+      })
+    })
+      .then(response => response.json())
+      .then((data) => {
+        console.log(data)
+        this.insertCarCard(data)
+      })
+
+      event.currentTarget.reset()
+  }
+};
